eeff552da0ba7b45bec78fda24368eaa12027557
Mauricio-NOTE <lanza.mauricio.qc@gmail.com>
2022-06-20 06:55:25 -0400

    Add project files.

---
+++ 1480-RunningSumOf1dArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/// <summary>
+/// Day 1 - Prefix Sum
+/// https://leetcode.com/study-plan/leetcode-75
+/// 1480. Running Sum of 1d Array
+/// https://leetcode.com/problems/running-sum-of-1d-array/
+/// </summary>
+namespace RunningSumOfArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            var input = new int[] { 1, 2, 3, 4 };
+
+            // Run
+            var result = RunningSum(input);
+
+            // Output
+            Console.WriteLine($"Expected output: [1,3,6,10]");
+
+            string output = "";
+            for (int i = 0; i < result.Length; i++)
+                output += (result[i] + " ");
+
+            Console.WriteLine("Real output: {0}", output);
+            Console.ReadLine();
+        }
+
+        public static int[] RunningSum(int[] nums)
+        {
+            int[] sums = new int[nums.Length];
+            int total = 0;
+
+            for(int index = 0; index < nums.Length; index++)
+            {
+                total += nums[index];
+                sums[index] = total;
+            }
+
+            return sums;
+        }
+    }
+}
---
+++ 724-FindPivotIndex/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+/// <summary>
+/// Day 1 - Prefix Sum
+/// https://leetcode.com/study-plan/leetcode-75
+/// 724. Find Pivot Index
+/// https://leetcode.com/problems/find-pivot-index/
+/// </summary>
+namespace FindPivotIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            int[] nums = { 1,7,3,6,5,6 };
+
+            // Run
+            int result = PivotIndex(nums);
+
+            // Output
+            Console.WriteLine("Expected output: 3"); ;
+            Console.WriteLine("Real output: {0}", result); ;
+            Console.ReadLine();
+        }
+        public static int PivotIndex(int[] nums)
+        {
+            int total = 0;
+            int leftSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                total += nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (leftSum == (total - leftSum - nums[i]))
+                    return i;
+
+                leftSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
