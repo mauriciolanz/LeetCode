78e399a61c82675d520bc857e56bce5f38446cf6
Mauricio-NOTE <lanza.mauricio.qc@gmail.com>
2022-06-20 07:59:32 -0400

    LeetCode 75 Path - Day 2 - 205. Isomorphic Strings

---
+++ 205-IsomorphicStrings/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/// <summary>
+/// Day 2 - String
+/// https://leetcode.com/study-plan/leetcode-75
+/// 205. Isomorphic Strings
+/// https://leetcode.com/problems/isomorphic-strings/
+/// </summary>
+namespace IsomorphicStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            string s = "paper";
+            string t = "title";
+            bool expectedResult = true;
+
+            // Run
+            bool result = IsIsomorphic(s, t);
+
+            // Output
+            Console.WriteLine("Expected result: {0}", expectedResult);
+            Console.WriteLine("Real result:     {0}", result);
+            Console.ReadLine();
+        }
+
+        public static bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length == t.Length)
+            {
+                var mapping = new Dictionary<char, char>();
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if(mapping.ContainsKey(s[i]))
+                    {
+                        if (mapping[s[i]] != t[i])
+                            return false;
+                    }
+                    else if(mapping.ContainsValue(t[i]))
+                    {
+                        // Contains the value, but it isn't isomorphic since it doesn't contain the key
+                        return false;
+                    }
+                    else
+                    {
+                        mapping.Add(s[i], t[i]);
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 205-IsomorphicStrings/Program_v1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+/// <summary>
+/// Day 2 - String
+/// https://leetcode.com/study-plan/leetcode-75
+/// 205. Isomorphic Strings
+/// https://leetcode.com/problems/isomorphic-strings/
+/// </summary>
+namespace IsomorphicStrings
+{
+    internal class Program_v1
+    {
+        static void Main_v1(string[] args)
+        {
+            // Input
+            string s = "paper";
+            string t = "title";
+            bool expectedResult = true;
+
+            // Run
+            bool result = IsIsomorphic(s, t);
+
+            // Output
+            Console.WriteLine("Expected result: {0}", expectedResult);
+            Console.WriteLine("Real result:     {0}", result);
+            Console.ReadLine();
+        }
+
+        public static bool IsIsomorphic(string s, string t)
+        {
+            if(s.Length == t.Length)
+            {
+                string substringS = "";
+                string substringT = "";
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    var positions = new List<int>();
+                    bool sHasRepeatedChar = false;
+                    bool tHasRepeatedChar = false;
+
+                    if (substringS.Contains(s[i]))
+                    {
+                        for(int j=0; j < substringS.Length; j++)
+                        {
+                            if(substringS[j] == s[i])
+                            {
+                                positions.Add(j);
+                            }
+                        }
+
+                        sHasRepeatedChar = true;
+                    }
+
+                    if (substringT.Contains(t[i]))
+                    {
+                        tHasRepeatedChar = true;
+
+                        foreach(int j in positions)
+                        {
+                            if (substringT[j] != t[i])
+                                return false;
+                        }
+                    }
+
+                    if (sHasRepeatedChar != tHasRepeatedChar)
+                        return false;
+
+                    substringS += s[i];
+                    substringT += t[i];
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
