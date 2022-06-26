4d77a8ffd5ec7fbfbf2995e8bf588e67a025696d
Mauricio-NOTE <lanza.mauricio.qc@gmail.com>
2022-06-25 22:35:31 -0400

    LeetCode 75 Path - Day 7 - 278. First Bad Version

---
+++ 278-FirstBadVersion/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/// <summary>
+/// Day 7 - Binary Search
+/// https://leetcode.com/study-plan/leetcode-75
+/// 278. First Bad Version
+/// https://leetcode.com/problems/first-bad-version/
+/// </summary>
+namespace FirstBadVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            var input = 9;
+
+            // Run
+            int result = FirstBadVersion(input);
+
+            // Output
+            Console.WriteLine("Expected result: {0}", 5);
+            Console.WriteLine("Real result:     {0}", result.ToString());
+            Console.ReadLine();
+        }
+        public static int FirstBadVersion(int n)
+        {
+            int left = 1;
+            int right = n;
+
+            while(left < right)
+            {
+                int pivot = left + ((right-left)/2);
+
+                if (IsBadVersion(pivot))
+                {
+                    right = pivot;
+                }
+                else
+                {
+                    left = pivot + 1;
+                }
+            }
+
+            return left;
+        }
+
+        public static bool IsBadVersion(int version)
+        {
+            if (version >= 5)
+                return true;
+
+            return false;
+        }
+    }
+}
